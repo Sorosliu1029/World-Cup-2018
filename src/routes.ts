718549b01d566ad5aff3a87a844614a35b7a8c39
Soros Liu <soros.liu1029@gmail.com>
2018-06-10 19:16:48 +0800

    Add health check

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import * as Router from 'koa-router'
+import { set } from 'lodash'
 
 const router = new Router()
 
@@ -10,13 +11,13 @@ async function about(ctx) {
   await ctx.render('about')
 }
 
-async function post(ctx) {
-  const body = ctx.request.body
-  console.log(body)
+async function health(ctx) {
+  ctx.body = set(ctx.request.body, 'success', true)
+  ctx.type = 'application/json'
 }
 
 router.get('/', game)
 router.get('/about', about)
-router.post('/', post)
+router.post('/health', health)
 
 export default router
